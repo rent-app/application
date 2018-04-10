3afce5dbeb96c9d218d6bfa022dca0fa1431b4af
Kenneth Seals-Nutt <k.sealsnutt@gmail.com>
2018-04-10 13:01:26 -0400

    FEAT: Added application configuration, scaffolding for DB, and deployment code with a basic landing page

---
+++ server/routes/index.js
@@ -0,0 +1,95 @@
+const membersController = require('../controllers').members;
+const fetch = require('node-fetch');
+const fs = require('fs');
+module.exports = (app, sessionChecker) => {
+  // Wrapper function to fetch
+  function _api(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err))
+    })
+  }
+
+
+  // route for Home-Page
+  app.get('/', sessionChecker, (req, res) => {
+      // res.redirect('/login');
+      res.render('full', {
+        page: function(){ return 'home'},
+        scripts: function(){ return 'home_scripts'},
+        links: function(){ return 'home_links'},
+        title: "Welcome",
+        nav: "home",
+      });
+  });
+  // route for Test-Page
+  app.get('/test', (req, res) => {
+      // res.redirect('/login');
+      res.render('base', {
+        page: function(){ return 'test'},
+        scripts: function(){ return 'test_scripts'},
+        links: function(){ return 'test_links'},
+        title: "Welcome",
+        nav: "test",
+      });
+  });
+
+  // route for Home-Page
+  app.get('/manifest', sessionChecker, (req, res) => {
+      // res.redirect('/login');
+      res.render('base', {
+        page: function(){ return 'manifest'},
+        scripts: function(){ return 'manifest_scripts'},
+        links: function(){ return 'manifest_links'},
+        title: "Manifest",
+        nav: "manifest",
+      });
+  });
+  // route for user signup
+  app.route('/signup')
+      .get(sessionChecker, (req, res) => {
+          res.render('signup');
+      })
+app.post('/signup', membersController.signup);
+
+  // route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+      res.render('login');
+    })
+app.post('/login', membersController.login)
+      // route for user's dashboard
+app.get('/dashboard', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.render('dashboard', {
+          user: req.session.user
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
+// route for user logout
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Rent Everything API!',
+  }));
+
+  app.post('/api/members', membersController.create);
+
+  // route for handling 404 requests(unavailable routes)
+  app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+  });
+
+
+};
