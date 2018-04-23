60f473dbf3d4a474620cc4b718904f36975f74fc
Kenneth Seals-Nutt <k.sealsnutt@gmail.com>
2018-04-23 03:38:13 -0400

    FIX: Error Debugging Caused by Async - Added error page - Reformatted Member controller to account for asynchronous loading

--- server/controllers/member.js
+++ server/controllers/member.js
@@ -1,86 +1,65 @@
 var Member = require('../models/member');
 const bcrypt = require('bcrypt');
 var salt = bcrypt.genSaltSync(10);
+var loadError = (req, res, msg) => {
+  res.status(401).redirect('/error?msg='+encodeURIComponent(msg))
+}
 
 // Handle Member creation on POST.
 exports.member_create = [
 
-    (req, res, next) => {
-        // Check to ensure inputs are valid. 
-        // Start with creating errors array which is added to for each error
-        errors = [];
+  (req, res, next) => {
+    // Check to ensure inputs are valid.
+    //check the email is not already registered
+    member = Member.find({ email: req.body.email.toLowerCase}, function(err, member) {
+      if (err) throw err;
+      if (member.length) loadError(req, res, 'Email is already exsists used')
+      return 'pass'
+    })
+    .then(output => {
+      //checks if the length of inputs is too long
+      lengthcheck = [req.body.name, req.body.email, req.body.pwd];
+      for(var i = 0; i < 3; i++)
+        if(lengthcheck[i].length > 50) loadError(req, res, 'Name, email and body must be below 50 characters');
 
-        //check the email is not already registered
-          member = Member.find({ email: req.body.email.toLowerCase}, function(err, member) {
-          console.log(member)
-            if (err) throw err;
-            if (member){
-              errors.push('Email is already exsists used')
-            };
-          });
-        
-        //checks if the length of inputs is too long
-        lengthcheck = [req.body.name, req.body.email, req.body.pwd];
-                for(var i = 0; i < lengthcheck.length; i++){
-        if(lengthcheck.length[i] > 50) {
-            errors.push('Name, email and body must be below 50 characters');
-          };
-        };
+      //check the email format is correct
+      if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email))
+        loadError(req, res, 'Name, email and body must be below 50 characters');
 
-        //check the email format is correct
-        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)) {
-          errors.push('Email format is incorrect');
-          };        
-        
-        //check if image_url is jpeg,jpg...
-        console.log(req.body.image_url.match(/\.(jpeg|jpg|gif|png)$/))
-        if(req.body.image_url.match(/\.(jpeg|jpg|gif|png)$/)){
-          errors.push('Image URL must be jpeg, jpg, gif or png. Leave blank for default image.')
-        };
+      //check if image_url is jpeg,jpg...
+      // console.log(req.body.image_url.match(/\.(jpeg|jpg|gif|png)$/))
+      if(req.body.image_url.match(/\.(jpeg|jpg|gif|png)$/))
+        loadError(req, res, 'Image URL must be jpeg, jpg, gif or png. Leave blank for default image.');
 
-        //check that password and password confirm match
-        if (req.body.pwd!=req.body.pwd_confirm){
-          errors.push('Passwords do not match');
-        };
-      
-      console.log('final check' + errors)
-      setTimeout(function(){
-      if (!errors){
+      //check that password and password confirm match
+      if (req.body.pwd!=req.body.pwd_confirm)
+        loadError(req, res, 'Passwords do not match.');
+
+      return 'pass'
+    })
+    .then(output => {
         // Hashes password
         var hash = bcrypt.hashSync(req.body.pwd, salt)
         // Generate random 10-digit number id
-
         // Enter new member into database
         Member.create({
             name: req.body.name,
             email: req.body.email,
-            image_url: req.body.image_url,
+            image_url: req.body.image_url || 'https://orig00.deviantart.net/10e3/f/2013/114/8/4/facebook_default_profile_picture___clone_trooper_by_captaintom-d62v2dr.jpg',
             hashed_password: hash,
-        });
-        console.log(req.body.name+" is now a member");
-        res.render('full',{
-          page: function(){return 'home'},
-          scripts: function(){return 'home_scripts'},
-          links: function(){return 'home_links'},
-          title: 'Welcome',
-          nav: 'home',
-          name: req.body.name
-       });
-      }
-      else {
-        console.log(errors);
-        res.render('full',{
-          page: function(){return 'home'},
-          scripts: function(){return 'home_scripts'},
-          links: function(){return 'home_links'},
-          title: 'Welcome',
-          nav: 'home',
-          errors: errors
-       });
-      }
-    }, 1000);
-            }];
-        
+        })
+        .then(output => {
+          // Load the listings page
+          console.log(req.body.name+" is now a member");
+          res.redirect('/inventory')
+        })
+        .catch(error => {
+          loadError(req, res, 'Oops.. Something Went wrong.');
+        })
+    })
+  }
+];
+
 // Test - Create Variables for member post
 exports.member_test_create = [
 
@@ -92,7 +71,7 @@ exports.member_test_create = [
         var email = 'Jest@email.com';
         var phone = '555-666-12345';
         var password = 'password';
-        
+
         // Hashes password
         var hash = bcrypt.hashSync(password, salt)
         // Generate random 10-digit number id
